b9f9fc054f74d77459eb01d090528494c3728654
ILHAM RIZQIAWAN, S.Pd. <ilhamrizqiawan@gmail.com>
2026-08-21 18:01:29 +0700

    feat: update KKO seed data to L1-L3

--- database/seeders/KkoSeeder.php
+++ database/seeders/KkoSeeder.php
@@ -3,54 +3,48 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\KkoMaster;  // <-- Tambahkan ini
+use App\Models\KkoMaster;
 
 class KkoSeeder extends Seeder
 {
     public function run(): void
     {
         $kkoData = [
-            // C1 - Mengingat
-            ['level' => 'C1', 'verb' => 'Menyebutkan'],
-            ['level' => 'C1', 'verb' => 'Mengutip'],
-            ['level' => 'C1', 'verb' => 'Mendefinisikan'],
-            ['level' => 'C1', 'verb' => 'Menghafal'],
-            ['level' => 'C1', 'verb' => 'Mengidentifikasi'],
-            
-            // C2 - Memahami
-            ['level' => 'C2', 'verb' => 'Menjelaskan'],
-            ['level' => 'C2', 'verb' => 'Menginterpretasi'],
-            ['level' => 'C2', 'verb' => 'Memberi contoh'],
-            ['level' => 'C2', 'verb' => 'Membedakan'],
-            ['level' => 'C2', 'verb' => 'Menyimpulkan'],
-            
-            // C3 - Menerapkan
-            ['level' => 'C3', 'verb' => 'Menggunakan'],
-            ['level' => 'C3', 'verb' => 'Menerapkan'],
-            ['level' => 'C3', 'verb' => 'Menghitung'],
-            ['level' => 'C3', 'verb' => 'Menunjukkan'],
-            ['level' => 'C3', 'verb' => 'Melaksanakan'],
-            
-            // C4 - Menganalisis
-            ['level' => 'C4', 'verb' => 'Menganalisis'],
-            ['level' => 'C4', 'verb' => 'Membandingkan'],
-            ['level' => 'C4', 'verb' => 'Mengorganisasi'],
-            ['level' => 'C4', 'verb' => 'Mengaudit'],
-            ['level' => 'C4', 'verb' => 'Mengkategorikan'],
-            
-            // C5 - Mengevaluasi
-            ['level' => 'C5', 'verb' => 'Mengkritisi'],
-            ['level' => 'C5', 'verb' => 'Menjustifikasi'],
-            ['level' => 'C5', 'verb' => 'Mempertimbangkan'],
-            ['level' => 'C5', 'verb' => 'Menilai'],
-            ['level' => 'C5', 'verb' => 'Merekomendasikan'],
-            
-            // C6 - Mencipta
-            ['level' => 'C6', 'verb' => 'Merancang'],
-            ['level' => 'C6', 'verb' => 'Mengkonstruksi'],
-            ['level' => 'C6', 'verb' => 'Mengabstraksi'],
-            ['level' => 'C6', 'verb' => 'Mengkombinasikan'],
-            ['level' => 'C6', 'verb' => 'Mengembangkan'],
+            // L1 - Mengingat & memahami
+            ['level' => 'L1', 'verb' => 'Menyebutkan'],
+            ['level' => 'L1', 'verb' => 'Mengutip'],
+            ['level' => 'L1', 'verb' => 'Mendefinisikan'],
+            ['level' => 'L1', 'verb' => 'Menghafal'],
+            ['level' => 'L1', 'verb' => 'Mengidentifikasi'],
+            ['level' => 'L1', 'verb' => 'Menjelaskan'],
+            ['level' => 'L1', 'verb' => 'Menginterpretasi'],
+            ['level' => 'L1', 'verb' => 'Memberi contoh'],
+            ['level' => 'L1', 'verb' => 'Membedakan'],
+            ['level' => 'L1', 'verb' => 'Menyimpulkan'],
+
+            // L2 - Menerapkan
+            ['level' => 'L2', 'verb' => 'Menggunakan'],
+            ['level' => 'L2', 'verb' => 'Menerapkan'],
+            ['level' => 'L2', 'verb' => 'Menghitung'],
+            ['level' => 'L2', 'verb' => 'Menunjukkan'],
+            ['level' => 'L2', 'verb' => 'Melaksanakan'],
+
+            // L3 - Menalar / HOTS
+            ['level' => 'L3', 'verb' => 'Menganalisis'],
+            ['level' => 'L3', 'verb' => 'Membandingkan'],
+            ['level' => 'L3', 'verb' => 'Mengorganisasi'],
+            ['level' => 'L3', 'verb' => 'Mengaudit'],
+            ['level' => 'L3', 'verb' => 'Mengkategorikan'],
+            ['level' => 'L3', 'verb' => 'Mengkritisi'],
+            ['level' => 'L3', 'verb' => 'Menjustifikasi'],
+            ['level' => 'L3', 'verb' => 'Mempertimbangkan'],
+            ['level' => 'L3', 'verb' => 'Menilai'],
+            ['level' => 'L3', 'verb' => 'Merekomendasikan'],
+            ['level' => 'L3', 'verb' => 'Merancang'],
+            ['level' => 'L3', 'verb' => 'Mengkonstruksi'],
+            ['level' => 'L3', 'verb' => 'Mengabstraksi'],
+            ['level' => 'L3', 'verb' => 'Mengkombinasikan'],
+            ['level' => 'L3', 'verb' => 'Mengembangkan'],
         ];
 
         foreach ($kkoData as $kko) {
